f463cfc18c2515d99555a3e7fd5f11a2fce82746
moeark-peralta <markperaltaasdgit@gmail.com>
2024-04-17 06:50:54 +0800

    create board function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 "use-strict";
 
-const gridContainer = document.querySelector(".grid-container");
+const board = document.querySelector(".board");
+const setButton = document.querySelector(".size-button");
+const sizeInput = document.getElementById("size-input");
+
+function createBoard(size) {
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+    console.log(i);
+  }
+}
+
+setButton.addEventListener("click", function () {
+  const size = parseInt(sizeInput.value);
+
+  createBoard(size);
+});
+
+createBoard();
